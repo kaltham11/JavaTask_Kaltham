f6437ac62fa8b069b730106dde79f17cfae3ac49
Kaltham <kaltham.aldifai@gmail.com>
2025-06-30 00:16:25 -0700

    print your cv on cmd

---
+++ KalthamCv.java
@@ -0,0 +1,40 @@
+public class KalthamCv {
+
+    public static void main(String[] args){
+        System.out.println("Kaltham Hamdoon AL Difai\n");
+        System.out.println("Al-Sharqiya North- AlMudhaibi\n");
+        System.out.println("Phone: +96878499961\n");
+        System.out.println("Email:kaltham.aldifai@gmail.com\n");
+        System.out.println("Graduated from University of Nizwa in 2022\n");
+        System.out.println("Major: computer Engineering\n");
+        System.out.println("""
+                About me: An enthusiastic graduate who is skilled in problem solving, maintaining
+                computer and computer networking. Achieved a high grade in my final
+                year project that is an IoT based safe assembly points with alert system for
+                disasters that help population and civil defense.
+                """);
+        System.out.println("""
+                             Internship:
+                                        Ministry of Information
+                                        Directorate General of Education
+                                        Sinaw Hospital
+                             """);
+        System.out.println("""
+                         skills:
+                         _Communication
+                         _Problem Solving
+                         _Team working
+                         _Technical Support
+                         _Bilingual: Arabic & English
+                         """);
+        System.out.println("Certificates:\n" +
+                           "              Computer Maintenance approved by the Ministry of Higher Education");
+
+
+
+
+
+
+
+    }
+}
